993a568584fd87952c7ca99f78c7b80e657ba98d
Pranil Kakade <pranilkakade111@gmail.com>
2021-02-23 00:23:22 +0530

    To check the maximum string in the array

--- Genrics/com.generics/FindMax.java
+++ Genrics/com.generics/FindMax.java
@@ -1,29 +1,33 @@
-
-public class FindMax 
-{
+public class FindMax
+{ 
  public static void main(String[] args) 
  {
 	Integer[] integerArr= {10,20,30,40,10};
 	Double[] doubleArr= {100.0,200.0,300.0,400.0};
 	Float[] floatArr= {200f,300f,500f,700f};
+	String[] stringArr= {"strawberry","pineapple","guvava","tim","do"};
 	printArr(integerArr);
 	printArr(doubleArr);
 	printArr(floatArr);
+	printArr(stringArr);
 	maxFind(integerArr);
 	maxFind(doubleArr);
 	maxFind(floatArr);
-
+	findmaxString(stringArr);
+	
+	
+	
 	
  }
  
  public static<E>void printArr(E[] element)
- {
+ {	 
 	 for(E i : element )
 		 System.out.print(i+" ");
 	 System.out.println();      
  }
  
- public static <T extends  Comparable <T>> void maxFind(T[] element)
+ public static  <T extends  Comparable <T>> void maxFind(T[] element)
  {
 	 T max=element[0];
 	 for(T i:element)
@@ -32,8 +36,18 @@ public class FindMax
 		 {
 			 max=i;
 		 }
-	 }System.out.println(max+" ");
-	 
-	 
+	 }System.out.println(max+" ");		
  }
+ 
+ public static <T extends Comparable<T>> void findmaxString(T[] elements) {
+	    T max = elements[0];
+	    for (T i : elements) 
+	    {
+	      if (Integer.valueOf(i.toString().length()).compareTo(Integer.valueOf(max.toString().length())) > 0) 
+	      {
+	        max = i;
+	      }
+	    }
+	    System.out.println("the maximum value in array is " + max);
+	  }
 }
